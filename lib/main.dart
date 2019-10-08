959352b757f260b51b00f32b7f1de66ba3ea56c5
webmobi59 <daxianjin59@hotmail.com>
2019-10-08 19:30:18 +0800

    [update] Change the theme.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hello World',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
